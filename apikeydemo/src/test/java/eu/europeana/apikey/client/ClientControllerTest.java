48c2a4ae572014e1b4ccf7f210777ea217e36fa1
Luthien-in-edhil <maike.dulk@europeana.eu>
2018-01-16 17:51:24 +0100

    finalising Jenkins Maven release config

--- apikeydemo/src/test/java/eu/europeana/apikey/client/ClientControllerTest.java
+++ apikeydemo/src/test/java/eu/europeana/apikey/client/ClientControllerTest.java
@@ -17,35 +17,30 @@
 
 package eu.europeana.apikey.client;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 
 /**
  * Created by luthien on 09/05/2017.
+ * TODO add junittests
  */
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc
 public class ClientControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
+//    @Autowired
+//    private MockMvc mvc;
 
     @Test
-    public void getHello() throws Exception {
+    public void contextLoad(){
+    }
+
+//    public void getHello() throws Exception {
 //        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 //                .andExpect(status().isOk())
 //                .andExpect(content().string(equalTo("The Apikey Client wishes you a good day.")));
-    }
+//    }
 }
